287d088d2f4da3b024711aaac5226609f428e375
Kent Slaney <kent@slaney.org>
2024-11-14 12:02:02 -0800

    caches embedding distances

--- src/cam-e2e/server.py
+++ src/cam-e2e/server.py
@@ -66,7 +66,7 @@ def bounded(lo, hi):
         return x
     return caller
 
-embedding_size = (224, 224) # rows, cols
+embedding_size = (480, 640) # rows, cols
 
 @app.route("/jobs/<job_name>/knn")
 @sanitize_job("job_name")
@@ -81,13 +81,36 @@ def knn_mask(job_name, x, y):
     out[y, x] = [0, 255, 0, 255]
     return encode_img(out)
 
+caches = {}
+def cached(**keys):
+    def decorator(f):
+        sig = inspect.signature(f)
+        @functools.wraps(f)
+        def wrapper(*a, **kw):
+            bound = sig.bind_partial(*a, **kw)
+            for k, v in keys.items():
+                if k in caches and len(caches[k]) == len(v) + 1 and all(
+                        caches[k][i] == bound.arguments[j]
+                        for i, j in enumerate(v)):
+                    bound.arguments[k] = caches[k][-1]
+            return f(*bound.args, **bound.kwargs)
+        return wrapper
+    return decorator
+
+def cache(*a, **kw):
+    assert len(kw) == 1
+    k, v = next(iter(kw.items()))
+    caches[k] = a + (v,)
+
 @app.route("/jobs/<job_name>/l2")
 @sanitize_job("job_name")
 @requires(
         x0=bounded(0., 1.), y0=bounded(0., 1.),
         x1=bounded(0., 1.), y1=bounded(0., 1.),
         t=bounded(0., 1.),  c=bounded(0., 1.), i=bounded(0, 1))
-def l2(job_name, x0, y0, x1, y1, t, c, i):
+@cached(embeddings=("job_name", "x0", "y0"))
+def l2(job_name, x0, y0, x1, y1, t, c, i, embeddings=None):
+    x_, y_ = x0, y0
     x0, y0 = int(x0 * embedding_size[1]), int(y0 * embedding_size[0])
     x1, y1 = int(x1 * embedding_size[1]), int(y1 * embedding_size[0])
     if i == 0:
@@ -98,19 +121,25 @@ def l2(job_name, x0, y0, x1, y1, t, c, i):
         if out.shape != embedding_size:
             out = cv2.resize(out, embedding_size[::-1])
         out = np.dstack((out, np.zeros(out.shape[:-1] + (1,))))
-    embeddings = np.load(jobs_path / job_name / "flat_embeddings.npy")
-    embeddings = embeddings.reshape(*embedding_size, -1)
+    if embeddings is None:
+        print('resolving distances')
+        embeddings = np.load(jobs_path / job_name / "flat_embeddings.npy")
+        embeddings = embeddings.reshape(*embedding_size, -1)
+        embeddings = (embeddings - embeddings[y0, x0]) ** 2
+        cache(job_name, x_, y_, embeddings=embeddings)
     if c != 0:
         im = np.load(jobs_path / job_name / "image_uint8.npy")
         if im.shape != embedding_size:
             im = cv2.resize(im, embedding_size[::-1])
+        im = (im - im[y0, x0]) ** 2
         embeddings = np.dstack((embeddings * (1 - c), im / 255 * c))
     if t != 0:
         pos = np.stack(np.meshgrid(
             np.linspace(0, 1, embedding_size[1]),
             np.linspace(0, 1, embedding_size[0])), -1)
+        pos = (pos - pos[y0, x0]) ** 2
         embeddings = np.dstack((embeddings * (1 - t), pos * t))
-    embeddings = np.sum((embeddings - embeddings[y0, x0]) ** 2, -1)
+    embeddings = np.sum(embeddings, -1)
     out[embeddings < embeddings[y1, x1], 3] = 255
     out[y0, x0] = [0, 255, 0, 255]
     out[y1, x1] = [0, 255, 0, 255]
